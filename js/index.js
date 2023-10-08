e9dd3c20063115be00ab29c1c23b90bdc496d04b
Adryel Santos <118002541+AdryelJr@users.noreply.github.com>
2023-10-08 14:05:05 -0300

    update code
    correção de bug

--- js/index.js
+++ js/index.js
@@ -1,17 +1,24 @@
-function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('active');
-}
+
+const profileIcon = document.getElementById('profileIcon');
 const sidebar = document.querySelector('.sidebar');
-const pesquisa = document.getElementById('pesquisa').addEventListener('click', function () {
-    sidebar.classList.remove('active');
+
+profileIcon.addEventListener('click', function(){
+    profileIcon.classList.toggle('active');
+    sidebar.classList.toggle('active');
+    const destaque = document.getElementById('destaque').addEventListener('click', function () {
+        sidebar.classList.remove('active');
+        profileIcon.classList.remove('active');
+    });
+    const formPrincipal = document.getElementById('form-principal').addEventListener('click', function () {
+        sidebar.classList.remove('active');
+        profileIcon.classList.remove('active');
+    });
+    const pesquisa = document.getElementById('pesquisa').addEventListener('click', function () {
+        sidebar.classList.remove('active');
+        profileIcon.classList.remove('active');
+    });
 });
-const destaque = document.getElementById('destaque').addEventListener('click', function () {
-    sidebar.classList.remove('active');
-});
-const formPrincipal = document.getElementById('form-principal').addEventListener('click', function () {
-    sidebar.classList.remove('active');
-});
+
 
 
 
